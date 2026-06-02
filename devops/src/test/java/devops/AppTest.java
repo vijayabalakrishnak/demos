6004ac1e59d0dac182b21f8ebc0e4eb981b06e64
vijay <vijayabalakrishnak@gmail.com>
2026-06-02 10:17:20 +0000

    done

---
+++ devops/src/test/java/devops/AppTest.java
@@ -0,0 +1,20 @@
+package devops;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
